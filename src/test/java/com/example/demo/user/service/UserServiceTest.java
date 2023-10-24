026df0155349b37606d31ca395e6cf0de79176c1
JinGyuGwak <wlsrb2695@naver.com>
2023-10-25 01:28:21 +0900

    userService 테스트코드 작성

--- src/test/java/com/example/demo/user/service/UserServiceTest.java
+++ src/test/java/com/example/demo/user/service/UserServiceTest.java
@@ -17,10 +17,13 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.example.demo.src.common.entity.BaseEntity.State.ACTIVE;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -39,9 +42,17 @@ public class UserServiceTest {
                 .password("269599zz")
                 .build();
     }
+    UserDto.PostUserDto dummyPostUserDto1(){
+        return UserDto.PostUserDto.builder()
+                .email("test1111@naver.com")
+                .name("고마츠나나")
+                .password("269599zz")
+                .build();
+    }
     @BeforeEach
     void before(){
         userService.createUser(dummyPostUserDto());
+        userService.createUser(dummyPostUserDto1());
     }
     @Test
     public void createUser_test(){
@@ -61,6 +72,43 @@ public class UserServiceTest {
         //then
         assertThat(user.getEmail()).isEqualTo("test2695@naver.com");
         assertThat(user.getName()).isEqualTo("카리나는 신이야");
+    }
+    @Test
+    public void deleteUser_test(){
+        //given
+        User userByEmail = funcUser.findUserByEmail("test2695@naver.com");
+        userService.deleteUser(userByEmail.getId());
+        //then
+        assertThrows(IllegalArgumentException.class, () -> {
+            funcUser.findUserByIdAndState(userByEmail.getId());
+        });
+    }
+    @Test
+    public void getUsers_test(){
+        //given
+        List<UserDto.GetUserRes> users = userService.getUsers();
+        //then
+        assertThat(users.size()).isEqualTo(2);
+    }
+    @Test
+    public void getUsersByEmail_test(){
+        //given
+        List<UserDto.GetUserRes> usersByEmail = userService.getUsersByEmail("test2695@naver.com");
+        //when
+        UserDto.GetUserRes getUserRes = usersByEmail.get(0);
+        //then
+        assertThat(usersByEmail.size()).isEqualTo(1);
+        assertThat(getUserRes.getEmail()).isEqualTo("test2695@naver.com");
+        assertThat(getUserRes.getName()).isEqualTo("카리나");
+    }
+    @Test
+    public void logIn_tesT(){
+        //given
+        UserDto.LoginUserDto loginUserDto = userService.logIn("test2695@naver.com", "269599zz");
+
+        assertThat(loginUserDto.getEmail()).isEqualTo("test2695@naver.com");
+        assertThat(loginUserDto.getPassword()).isEqualTo("269599zz");
+        assertNotNull(loginUserDto.getJwt());
     }
 
 
