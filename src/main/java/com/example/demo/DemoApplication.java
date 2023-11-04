c07c76a831662b8329fa4e8195faee6ebbc8b4ce
JinGyuGwak <wlsrb2695@naver.com>
2023-11-04 21:58:08 +0900

    cicd test now

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication // 갑자기 안 돼
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DemoApplication.class, args);
     }
 
