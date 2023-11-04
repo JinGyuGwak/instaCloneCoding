b64bb0d4f78a52e593ee9737e8d904fc713692b3
JinGyuGwak <wlsrb2695@naver.com>
2023-11-04 12:14:27 +0900

    cicd 테스트

--- src/main/java/com/example/demo/src/util/LoginUtil.java
+++ src/main/java/com/example/demo/src/util/LoginUtil.java
@@ -4,7 +4,7 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-public class LoginUtil {
+public class LoginUtil { //1
     public static String getLoginEmail() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
