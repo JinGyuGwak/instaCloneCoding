9eb79cb7f59f71601b44be0af759a06d8c4d34ee
JinGyuGwak <wlsrb2695@naver.com>
2023-11-04 23:46:34 +0900

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class DemoApplication {
 
     public static void main(String[] args) {
-        //아 왜 안 돼!!!!
         SpringApplication.run(DemoApplication.class, args);
     }
 
